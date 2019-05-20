42417d0b4b5bd9b86877f4288f7c23e758a2c49d
danie <danie@DESKTOP-M2H76EP>
2019-05-20 13:19:37 +0200

    fixed basketball with strategy pattern.

--- PRG08-basketball-strategy-master/dev/basketball.ts
+++ PRG08-basketball-strategy-master/dev/basketball.ts
@@ -1,27 +1,29 @@
 class BasketBall extends Ball {
-behaviour: Behaviour;
+    behaviour: Behaviour;
     constructor(minWidth: number, maxWidth: number) {
         super(minWidth, maxWidth)
         this.behaviour = new EarthBouncing(this)
-        this.behaviour= new MoonBouncing(this)
 
 
     }
 
     public update(): void {
         if (this.x < window.innerWidth / 2) {
-           this.draw()
-           this.behaviour.update()
-
-        } else {
+            this.behaviour = new EarthBouncing(this)
             this.draw()
             this.behaviour.update()
 
-            }
+        } else {
+            this.behaviour = new MoonBouncing(this)
+            this.draw()
+            this.behaviour.update()
 
-           
         }
     }
+
+
+}
+
 
 
 window.customElements.define("basketball-component", BasketBall)
